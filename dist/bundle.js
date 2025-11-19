41fbd091c39da3382082701dd8596d7d95548d4a
matdombrock <dombrock.mathieu@gmail.com>
2025-11-19 02:58:06 -0800

    update bundle

--- dist/bundle.js
+++ dist/bundle.js
@@ -222,7 +222,7 @@ class PazUI {
       length: document.getElementById("in-length"),
       revision: document.getElementById("in-revision"),
       hash: document.getElementById("out-hash"),
-      note: document.getElementById("in-note"),
+      notes: document.getElementById("in-notes"),
       minIterations: document.getElementById("in-min-iterations"),
       algorithm: document.getElementById("in-algorithm"),
       append: document.getElementById("in-append"),
@@ -241,7 +241,7 @@ class PazUI {
     this.elements.special.addEventListener("input", () => this.computeHash());
     this.elements.length.addEventListener("input", () => this.computeHash());
     this.elements.revision.addEventListener("input", () => this.computeHash());
-    this.elements.note.addEventListener("input", () => this.computeHash());
+    this.elements.notes.addEventListener("input", () => this.computeHash());
     this.elements.minIterations.addEventListener("input", () => this.computeHash());
     this.elements.append.addEventListener("input", () => this.computeHash());
     this.elements.algorithm.addEventListener("input", () => this.computeHash());
@@ -268,7 +268,7 @@ class PazUI {
     this.elements.special.value = urlParams.get("special") || this.elements.special.value;
     this.elements.length.value = urlParams.get("length") || this.elements.length.value;
     this.elements.revision.value = urlParams.get("revision") || this.elements.revision.value;
-    this.elements.note.value = urlParams.get("note") || this.elements.note.value;
+    this.elements.notes.value = urlParams.get("notes") || this.elements.notes.value;
     this.elements.minIterations.value = urlParams.get("minIterations") || this.elements.minIterations.value;
     this.elements.append.value = urlParams.get("append") || this.elements.append.value;
     this.elements.algorithm.value = urlParams.get("algorithm") || this.elements.algorithm.value;
@@ -279,7 +279,7 @@ class PazUI {
     url.searchParams.set("special", site.special);
     url.searchParams.set("length", site.length.toString());
     url.searchParams.set("revision", site.revision.toString());
-    url.searchParams.set("note", this.elements.note.value);
+    url.searchParams.set("notes", this.elements.notes.value);
     url.searchParams.set("minIterations", site.minIterations.toString());
     url.searchParams.set("append", site.append);
     url.searchParams.set("algorithm", site.algorithm);
@@ -291,7 +291,7 @@ class PazUI {
     url.searchParams.delete("special");
     url.searchParams.delete("length");
     url.searchParams.delete("revision");
-    url.searchParams.delete("note");
+    url.searchParams.delete("notes");
     url.searchParams.delete("minIterations");
     url.searchParams.delete("append");
     url.searchParams.delete("algorithm");
@@ -303,7 +303,7 @@ class PazUI {
       special: this.elements.special.value,
       length: parseInt(this.elements.length.value, 10),
       revision: parseInt(this.elements.revision.value, 10),
-      note: this.elements.note.value,
+      note: this.elements.notes.value,
       minIterations: parseInt(this.elements.minIterations.value, 10),
       algorithm: this.elements.algorithm.value,
       append: this.elements.append.value
